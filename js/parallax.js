fbcbb605ebe5ddc505803602bd476b6d7da51144
James Callow <jamescallow@jc-mbp.local>
2017-04-05 17:16:29 -0400

    Initial commit

---
+++ js/parallax.js
@@ -0,0 +1,42 @@
+$(window).resize(function(){
+    $window = $(window);
+    if( $window .width() > 800){
+        var $window = $(window);
+
+        $('section[data-type="background"]').each(function(){
+            var $bgobj = $(this);
+            $(window).scroll(function() {
+            // Scroll the background at var speed
+            // the yPos is a negative value because we're scrolling it UP!
+            var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        });
+
+        });
+    }
+});
+
+$(document).ready(function(){
+    $window = $(window);
+    if( $window.width() > 800){
+
+        var $window = $(window);
+
+        $('section[data-type="background"]').each(function(){
+            var $bgobj = $(this);
+            $(window).scroll(function() {
+            // Scroll the background at var speed
+            // the yPos is a negative value because we're scrolling it UP!
+            var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        });
+
+        });
+    }
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+#My ePortfolio
+
+To view live site, open the following URL:  
+[http://callowjames.com](http://callowjames.com)  
